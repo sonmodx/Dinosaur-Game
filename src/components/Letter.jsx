751c80637e4bc899671db63462f23f3b65f0c634
sonmodx <sonmodx@gmail.com>
2023-10-22 14:30:45 +0700

    Check is user focus input and Adding blink animation

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles/letter.css";
-const Letter = ({ letter, index, userInput }) => {
+const Letter = ({ letter, index, userInput, isFocus }) => {
   const indexCaret = userInput?.length;
 
   return (
     <span
       className={`letter ${userInput?.at(index) === letter && "passed"} ${
-        indexCaret === index && "current"
+        indexCaret === index && isFocus && "current"
       }`}
     >
       {letter}
--- src/components/TypingTest.jsx
+++ src/components/TypingTest.jsx
@@ -6,6 +6,8 @@ const TypingTest = () => {
   const [userInput, setUserInput] = useState([]);
   const correctText = "but school everyday is good for you too my love";
   const [isFinish, setIsFinish] = useState(false);
+  const [isStart, setIsStart] = useState(false);
+  const [isFocus, setIsFocus] = useState(true);
   const [selectSeconds, setSelectSeconds] = useState(5);
   const [seconds, setSeconds] = useState(5);
 
@@ -18,20 +20,23 @@ const TypingTest = () => {
 
   useEffect(() => {
     let _sec = selectSeconds;
+    if (!isStart) return;
+    console.log("START");
     const time = setInterval(() => {
+      _sec -= 1;
+      setSeconds(_sec);
       if (isFinish) {
         _sec = 0;
       }
       if (_sec === 0) {
         setIsFinish(true);
+        setIsStart(false);
         clearInterval(time);
         return;
       }
-      _sec -= 1;
-      setSeconds(_sec);
     }, 1000);
     return () => clearInterval(time);
-  }, [isFinish]);
+  }, [isFinish, isStart]);
 
   useEffect(() => {
     const userInputString = userInput.join("");
@@ -55,11 +60,16 @@ const TypingTest = () => {
     <div className="typing-test">
       <p className="time">{seconds}</p>
 
-      <div className="wrapper">
+      <div className={`wrapper ${!isFocus && "blur"}`}>
         <input
           type="text"
-          onInput={(e) => setUserInput(e.target.value.split(""))}
+          onInput={(e) => {
+            setUserInput(e.target.value.split(""));
+            setIsStart(true);
+          }}
           autoFocus={true}
+          onBlur={() => setIsFocus(false)}
+          onFocus={() => setIsFocus(true)}
         />
         <div className="show-text">
           {correctText?.split("")?.map((letter, idx) => (
@@ -68,10 +78,16 @@ const TypingTest = () => {
               letter={letter}
               index={idx}
               userInput={userInput}
+              isFocus={isFocus}
             />
           ))}
         </div>
       </div>
+      {!isFocus ? (
+        <p className="type-to-start">Click to focus!</p>
+      ) : (
+        !isStart && <p className="type-to-start">Type to start!</p>
+      )}
     </div>
   );
 };
